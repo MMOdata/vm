1b0eee3fa263ea71e957522316fa9e74881a92fe
novis <87120469+Novisai@users.noreply.github.com>
2024-03-25 11:46:34 +0000

    Create experienceController.js

---
+++ New folder/backend/controllers/experienceController.js
@@ -0,0 +1,12 @@
+const Experience = require('../models/Experience');
+
+exports.getExperiences = async (req, res) => {
+  try {
+    const experiences = await Experience.find();
+    res.json(experiences);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// Implement other CRUD operations as needed
